138dd3e0962d13a0ea1d7c15c6aa92cac6fdf03f
lucianomerlo11 <merlo1411@gmail.com>
2022-01-23 20:47:27 -0300

    Estructura de la pagina reserva lista

---
+++ codigo/codigo.js
@@ -0,0 +1,13 @@
+let fecha = new Date();
+
+let diaSiguiente = fecha.getDate()
+let mes = fecha.getMonth() + 1;
+let año = fecha.getFullYear();
+let fechaCompleta = diaSiguiente + "/" + mesSiguiente + "/" + año;
+
+
+let respuesta = prompt("Si desea una clase de prueba, ingrese su nombre: ");
+
+if (respuesta != "") {
+    alert(`Excelente ${respuesta} . Lo esperamos mañana ${diaSiguiente + 1}/${mes}/${año}`);
+}
