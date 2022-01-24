66a9d04b40baf42ec0ad03a065abc615df424e05
lucianomerlo11 <merlo1411@gmail.com>
2022-01-24 20:04:55 -0300

    Commit 2: Agrego la pagina de reservas

--- codigo/codigo.js
+++ codigo/codigo.js
@@ -3,7 +3,7 @@ let fecha = new Date();
 let diaSiguiente = fecha.getDate()
 let mes = fecha.getMonth() + 1;
 let año = fecha.getFullYear();
-let fechaCompleta = diaSiguiente + "/" + mesSiguiente + "/" + año;
+let fechaCompleta = diaSiguiente + "/" + mes + "/" + año;
 
 
 let respuesta = prompt("Si desea una clase de prueba, ingrese su nombre: ");
