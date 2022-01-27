92f7b03082ad3d8ff12f9adfc6809fd1cfd15c2e
lucianomerlo11 <merlo1411@gmail.com>
2022-01-27 00:11:24 -0300

    Commit 3: Proyecto listo para formar parte de la entrega del desafio 2

--- codigo/codigo.js
+++ codigo/codigo.js
@@ -1,15 +1,27 @@
+let respuesta = prompt("Si desea una clase de prueba, ingrese su nombre: ");
 let fecha = new Date();
 
-let diaSiguiente = fecha.getDate()
-let mes = fecha.getMonth() + 1;
-let año = fecha.getFullYear();
-let fechaCompleta = diaSiguiente + "/" + mes + "/" + año;
+const fechaSiguiente = obtenerFechaSiguiente();
+const quiereClaseDePrueba = validarClaseDePrueba(respuesta)
 
+if (quiereClaseDePrueba) {
+    alert(`Excelente ${respuesta} . ${fechaSiguiente}`);
+}
 
-let respuesta = prompt("Si desea una clase de prueba, ingrese su nombre: ");
+function obtenerFechaSiguiente() {
 
-if (respuesta != "" || respuesta != null) {
-    alert(`Excelente ${respuesta} . Lo esperamos mañana ${diaSiguiente + 1}/${mes}/${año}`);
-} else {
-    alert("Esperamos que vengas a nustro gimnasio");
+    let diaSiguiente = fecha.getDate()
+    let mes = fecha.getMonth() + 1;
+    let año = fecha.getFullYear();
+
+    return `Lo esperamos mañana ${diaSiguiente + 1}/${mes}/${año}`;
+}
+
+function validarClaseDePrueba(respuesta) {
+    if (respuesta != "" || respuesta != null) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
