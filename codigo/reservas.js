06591622bfc3f84ad2087c3415107be26230f799
lucianomerlo11 <merlo1411@gmail.com>
2022-02-09 23:06:29 -0300

    Agrego archivo turnos.js

--- codigo/reservas.js
+++ codigo/reservas.js
@@ -1,104 +1,160 @@
-class DetalleReserva {
-    constructor(diaReserva, claseReserva, horarioReserva, turnoReserva) {
-        this.diaReserva = diaReserva;
-        this.claseReserva = claseReserva;
-        this.horarioReserva = horarioReserva;
-        this.turnoReserva = turnoReserva
-    }
-}
 
 
-class Reserva {
-    constructor(fechaReserva, horaReserva, detalleReserva) {
-        this.fechaReserva = fechaReserva;
-        this.horaReserva = horaReserva;
-        this.detalleReserva = detalleReserva;
-    }
-}
+const contenedorTurno = document.getElementById("contenedor-turnos");
 
 
-let btnConsulta = document.getElementById("btn_consultar");
-let cupoReserva = 14;
-let arrayReservas = [];
+mostrarTurnos(turnos);
 
+function mostrarTurnos(array) {
+    array.forEach(turno => {
+        let div = document.createElement("div");
+        div.className = "turno";
+        div.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">${turno.clase}</h5>
+              <p class="card-text">Día: ${turno.dia}</p>
+              <p class="card-text">Turno: ${turno.turno}</p>
+              <p class="card-text">Horario: ${turno.horario}</p>
+              <a href="#" class="btn btn-primary">Reservar</a>
+            </div>
+        </div>`
 
-function consultar() {
-
-    let ingresoDatos = false;
-
-    let dia = document.getElementById("dia").value;
-    let turno = document.getElementById("turno").value;
-    let hora = document.getElementById("hora").value;
-    let tipoClase = document.getElementById("tipoClase").value;
-
-    if (dia == "") {
-        alert("Seleccione dia")
-    } else if (turno == "") {
-        alert("Seleccione turno")
-    } else if (hora == "") {
-        alert("Seleccione hora")
-    } else if (tipoClase == "") {
-        alert("Seleccione tipoClase")
-    } else {
-        ingresoDatos = true;
-    }
-
-    return ingresoDatos;
+        contenedorTurno.appendChild(div);
+    });
 }
 
 
 
-function generarReserva() {
-
-    if (consultar()) {
-
-        // let cupoReserva = 14;
-        let dia = document.getElementById("dia").value;
-        if (dia == 'maniana') {
-            dia = "Mañana";
-        }
-        let turno = document.getElementById("turno").value;
-        let hora = document.getElementById("hora").value;
-        let tipoClase = document.getElementById("tipoClase").value;
-
-
-        document.getElementById("tituloActividad").innerHTML = `Clase: ${tipoClase}`;
-        document.getElementById("turnoReserva").innerHTML = `Turno: ${turno}`;
-        document.getElementById("diaReserva").innerHTML = `Dia: ${dia}`;
-        document.getElementById("cupo").innerHTML = `Cupo: ${cupoReserva}`;
-        document.getElementById("horaActividad").innerHTML = `Hora: ${hora}`;
-    } else {
-        alert("No se llenaron los campos de la reserva");
-    }
-
-}
 
 
 
-function reservar() {
-    alert("Reserva confirmada");
-    cupoReserva = cupoReserva - 1;
-    document.getElementById("cupo").innerHTML = `Cupo: ${cupoReserva}`
-    document.getElementById("btnReservar").innerHTML = "Cancelar";
 
-    let fecha = new Date();
 
-    arrayReservas.push(
-        new Reserva(
-            `${fecha.getDate() + 1}-${fecha.getMonth()}-${fecha.getFullYear()}`,
-            `${fecha.getHours()}:${fecha.getMinutes()}`,
-            new DetalleReserva(
-                document.getElementById("diaReserva").innerHTML,
-                document.getElementById("tituloActividad").innerHTML,
-                document.getElementById("horaActividad").innerHTML,
-                document.getElementById("turnoReserva").innerHTML
-            )
-        )
-    );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class DetalleReserva {
+//     constructor(diaReserva, claseReserva, horarioReserva, turnoReserva) {
+//         this.diaReserva = diaReserva;
+//         this.claseReserva = claseReserva;
+//         this.horarioReserva = horarioReserva;
+//         this.turnoReserva = turnoReserva
+//     }
+// }
+
+
+// class Reserva {
+//     constructor(fechaReserva, horaReserva, detalleReserva) {
+//         this.fechaReserva = fechaReserva;
+//         this.horaReserva = horaReserva;
+//         this.detalleReserva = detalleReserva;
+//     }
+// }
+
+
+// let btnConsulta = document.getElementById("btn_consultar");
+// let cupoReserva = 14;
+// let arrayReservas = [];
+
+
+// function consultar() {
+
+//     let ingresoDatos = false;
+
+//     let dia = document.getElementById("dia").value;
+//     let turno = document.getElementById("turno").value;
+//     let hora = document.getElementById("hora").value;
+//     let tipoClase = document.getElementById("tipoClase").value;
+
+//     if (dia == "") {
+//         alert("Seleccione dia")
+//     } else if (turno == "") {
+//         alert("Seleccione turno")
+//     } else if (hora == "") {
+//         alert("Seleccione hora")
+//     } else if (tipoClase == "") {
+//         alert("Seleccione tipoClase")
+//     } else {
+//         ingresoDatos = true;
+//     }
+
+//     return ingresoDatos;
+// }
+
+
+
+// function generarReserva() {
+
+//     if (consultar()) {
+
+//         // let cupoReserva = 14;
+//         let dia = document.getElementById("dia").value;
+//         if (dia == 'maniana') {
+//             dia = "Mañana";
+//         }
+//         let turno = document.getElementById("turno").value;
+//         let hora = document.getElementById("hora").value;
+//         let tipoClase = document.getElementById("tipoClase").value;
+
+
+//         document.getElementById("tituloActividad").innerHTML = `Clase: ${tipoClase}`;
+//         document.getElementById("turnoReserva").innerHTML = `Turno: ${turno}`;
+//         document.getElementById("diaReserva").innerHTML = `Dia: ${dia}`;
+//         document.getElementById("cupo").innerHTML = `Cupo: ${cupoReserva}`;
+//         document.getElementById("horaActividad").innerHTML = `Hora: ${hora}`;
+//     } else {
+//         alert("No se llenaron los campos de la reserva");
+//     }
+
+// }
+
+
+
+// function reservar() {
+//     alert("Reserva confirmada");
+//     cupoReserva = cupoReserva - 1;
+//     document.getElementById("cupo").innerHTML = `Cupo: ${cupoReserva}`
+//     document.getElementById("btnReservar").innerHTML = "Cancelar";
+
+//     let fecha = new Date();
+
+//     arrayReservas.push(
+//         new Reserva(
+//             `${fecha.getDate() + 1}-${fecha.getMonth()}-${fecha.getFullYear()}`,
+//             `${fecha.getHours()}:${fecha.getMinutes()}`,
+//             new DetalleReserva(
+//                 document.getElementById("diaReserva").innerHTML,
+//                 document.getElementById("tituloActividad").innerHTML,
+//                 document.getElementById("horaActividad").innerHTML,
+//                 document.getElementById("turnoReserva").innerHTML
+//             )
+//         )
+//     );
 
 
     
 
-    console.log(arrayReservas);
-}
+//     console.log(arrayReservas);
+// }
 
--- codigo/validacionFormLogin.js
+++ codigo/validacionFormLogin.js
@@ -20,17 +20,29 @@ let usuario3 = new Usuario("pepers", "larioja123");
 let usuariosRegistrados = [usuario1, usuario2, usuario3];
 
 
+let btnIngresar = document.getElementById("btnIngresar");
+
 
 function validarUsuario() {
     //ENTRADAS
     let usuario = document.getElementById("usuario").value;
     let password = document.getElementById("password").value;
     
-    if (usuario == "" || password == "") alert("No ha ingresado usuario o contraseña");
-    else if (usuariosRegistrados.some((usr) => usr.nombreUsuario == usuario && usr.contrasenia == password)) alert("Bienvenido");
-    else alert("Usuario no registrado");
+    if (usuario == "" || password == "") {
+        //No se ingresan datos
+        alert("No ha ingresado usuario o contraseña");
+    }
+    else if (!usuariosRegistrados.some((usr) => usr.nombreUsuario == usuario && usr.contrasenia == password)){
+        //El usuario no existe y no puede ingresar
+        alert("Usuario no registrado");
+    }
+    else{
+        //El usuario existe y puede ingresar
+    alert("Bienvenivo")        
+    }
 
 }
+
 
 
 
